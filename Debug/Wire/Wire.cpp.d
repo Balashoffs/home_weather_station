fbcd8f5ba7395e7167508c9e505058d345dc4903
Balashoffs <balashov87aleksandr@gmail.com>
2019-10-06 23:41:11 +0700

    edit gitignore

--- Debug/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\User\AppData\Local\Temp\VMBuilds\home_weather_station\mega_atmega2560\Debug\Wire\Wire.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\Wire.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\utility/twi.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h
